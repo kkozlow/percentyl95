13f43509f20fadd898c89e4cfd63b2087336c70f
Krzysztof Kozłowski <kk@KK.(none)>
2012-09-28 10:00:06 +0200

    aktualizacja env=production log_level i assets_precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Percentyl95::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -31,7 +31,7 @@ Percentyl95::Application.configure do
   # config.force_ssl = true
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
+  config.log_level = :warn
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
@@ -57,4 +57,7 @@ Percentyl95::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  #config.log_level = :debug
+
 end
